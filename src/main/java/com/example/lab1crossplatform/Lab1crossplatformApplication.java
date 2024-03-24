191ce670d42a7ffabb18be55f6ddac2455da0840
Maxym <mvtutunyk2005@gmail.com>
2024-03-24 17:28:45 +0200

    first commit

---
+++ src/main/java/com/example/lab1crossplatform/Lab1crossplatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab1crossplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1crossplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1crossplatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab1crossplatform/Lab1crossplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1crossplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1crossplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
